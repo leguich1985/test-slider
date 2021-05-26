9e3f8e0775b593c5c5f59751b36b76c4ed0e6773
leguich1985 <leguich@mail.ru>
2021-05-27 01:10:06 +0300

    slider

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Slider from "react-slick";
+import { useEffect, useState } from "react";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
+
+const El = ({ card }) => (
+  <div
+    style={{
+      height: "200px",
+      width: "200px",
+      backgroundImage: `url(${card.image})`,
+    }}
+  >
+    <h1>{card.description}</h1>
+  </div>
+);
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 1000,
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3001/cards").then((res) =>
+      res.json().then((res) => setData(res))
+    );
+  }, []);
+
+  console.log(data);
+
+  if (!data) return null;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Slider {...settings}>
+        {/* {data.map((card) => (
+          <El key={card.id} card={card} />
+        ))} */}
+
+        <El card={data[0]} />
+        <El card={data[1]} />
+        <El card={data[2]} />
+        <El card={data[3]} />
+        <El card={data[4]} />
+      </Slider>
     </div>
   );
 }
